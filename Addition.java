874c251719f6d829802339995a2648bdd0c49a20
lax <mlhit5@gmail.com>
2026-04-19 23:50:19 +0530

    third commit

--- Addition.java
+++ Addition.java
@@ -1,7 +1,7 @@
 //addition program
 import java.util.Scanner;
 
-class Addition {
+public class Addition {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
