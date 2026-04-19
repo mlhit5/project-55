9167b24a75a1c465f826afcd2b37130f8969ec66
lax <mlhit5@gmail.com>
2026-04-19 23:33:36 +0530

    first commit

---
+++ Str.java
@@ -0,0 +1,11 @@
+import java.io.*;
+public class Str
+{
+public static void main(String args[])
+{
+String s="hello";
+String s1=s.concat("world");
+System.out.println(s);
+System.out.println(s1);
+}
+}
---
+++ Sum.java
@@ -0,0 +1,12 @@
+import java.io.*;
+class sum
+{
+public static void main(String args[])
+{
+int a=Integer.parseInt(args[0]);
+int b=Integer.parseInt(args[1]);
+int c=a+b;
+System.out.println("The addition of "+a+" and "+b+" is "+c);
+}
+}
+ 
